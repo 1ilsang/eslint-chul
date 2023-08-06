27fbf2cf9b4381eec4f83e0df4afb75ad003ee7b
1ilsang <1ilsang@naver.com>
2023-08-06 15:26:12 +0900

    feat: Add chul rules

---
+++ tests/lib/rules/chul.js
@@ -0,0 +1,35 @@
+/**
+ * @fileoverview my plugin settings
+ * @author 1ilsang
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/chul"),
+  RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("chul", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+  ],
+
+  invalid: [
+    {
+      code: "var data = getData123();",
+      errors: [
+        {
+          message: "[getData123()] 함수에 숫자..?",
+          type: "CallExpression",
+        },
+      ],
+    },
+  ],
+});
